339c5b6a846dcbe53ae80171b355f9f65570af74
fernandoariel9-jpg <fernandoariel9@gmail.com>
2025-09-25 17:18:55 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React, { useState, useEffect } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { motion, AnimatePresence } from "framer-motion";
-import Login from "./Login";
 import RegistroUsuario from "./RegistroUsuario";
 import Panel from "./Panel";
 
