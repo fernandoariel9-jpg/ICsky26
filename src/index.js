539fc787e758f54beae0c872c8f169421f7c220e
fernandoariel9-jpg <fernandoariel9@gmail.com>
2025-09-25 12:28:19 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,17 @@
-import React, { useState } from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import RegistroUsuario from "./RegistroUsuario";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-function Main() {
-  const [registrado, setRegistrado] = useState(false);
-  const [usuario, setUsuario] = useState(localStorage.getItem("usuario") || "");
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-  if (!registrado && !usuario) {
-    return <RegistroUsuario onRegistrado={(nombre) => {
-      localStorage.setItem("usuario", nombre);
-      setUsuario(nombre);
-      setRegistrado(true);
-    }} />;
-  }
-
-  return <App />;
-}
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Main />);
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
