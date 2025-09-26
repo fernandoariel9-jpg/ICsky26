764093c8183a2f39e606e83e5b25aabb8dd8a318
fernandoariel9-jpg <fernandoariel9@gmail.com>
2025-09-26 17:50:46 -0300

    Update UsuarioLogin.js

--- src/UsuarioLogin.js
+++ src/UsuarioLogin.js
@@ -30,6 +30,11 @@ export default function UsuarioLogin({ onLogin, switchToRegister }) {
 
   return (
     <div className="p-4 max-w-md mx-auto mt-20">
+    <img
+          src="/logosmall.png"
+          alt="Logo"
+          className="mx-auto mb-4 w-24 h-auto"
+        />
       <h1 className="text-2xl font-bold text-center mb-4">Login de Usuario</h1>
       <form onSubmit={handleLogin} className="flex flex-col space-y-3">
         <input type="text" placeholder="Nombre de usuario" className="w-full p-2 border rounded" value={usuario} onChange={e => setUsuario(e.target.value)} required/>
