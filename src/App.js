07c21b050be3772cf03c99f004e20724c6e9c8e8
fernandoariel9-jpg <fernandoariel9@gmail.com>
2025-09-26 17:57:42 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -117,6 +117,11 @@ function PanelLogin({ onLogin }) {
 
   return (
     <div className="p-4 max-w-md mx-auto mt-20">
+      <img
+          src="/logosmall.png"
+          alt="Logo"
+          className="mx-auto mb-4 w-24 h-auto"
+        />
       <h1 className="text-2xl font-bold text-center mb-4">
         🔒 Acceso Panel de Supervisión
       </h1>
