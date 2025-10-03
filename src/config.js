f0aa40c70fc09086909311cbbbb7f50770b0844e
fernandoariel9-jpg <fernandoariel9@gmail.com>
2025-10-03 11:52:48 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,11 @@
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:4000";
-export default API_URL;
+const API_BASE_URL = "https://sky26.onrender.com";
+
+export const ENDPOINTS = {
+  USUARIOS: `${API_BASE_URL}/usuarios`,
+  LOGIN_USUARIOS: `${API_BASE_URL}/usuarios/login`,
+  PERSONAL: `${API_BASE_URL}/personal`,
+  LOGIN_PERSONAL: `${API_BASE_URL}/personal/login`,
+  AREAS: `${API_BASE_URL}/areas`,
+  SERVICIOS: `${API_BASE_URL}/servicios`,
+  TAREAS: `${API_BASE_URL}/tareas`,
+};
