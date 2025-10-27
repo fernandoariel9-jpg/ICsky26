e3df59e0a723754eb9e8b0d791aae1024b1f1533
fernandoariel9-jpg <fernandoariel9@gmail.com>
2025-10-27 11:34:37 -0300

    Update PanelLogin.js

--- src/PanelLogin.js
+++ src/PanelLogin.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { motion, AnimatePresence } from "framer-motion";
-import AsistenteIA from "./AsistenteIA";
+import AsistenteIAFlotante from "./AsistenteIAFlotante";
 
 import RegistroUsuario from "./RegistroUsuario";
 import RegistroPersonal from "./RegistroPersonal";
