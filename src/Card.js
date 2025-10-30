4ae34e64530899f1175c2aa4861568def756df84
fernandoariel9-jpg <fernandoariel9@gmail.com>
2025-10-30 09:04:58 -0300

    Create Card.js

---
+++ src/Card.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Card({ children, className = "" }) {
+  return (
+    <div className={`bg-white rounded-2xl shadow-md p-4 ${className}`}>
+      {children}
+    </div>
+  );
+}
