b42ea47a28a94a06522e935078a86c1fb495f2fd
fernandoariel9-jpg <fernandoariel9@gmail.com>
2025-11-18 12:43:03 -0300

    Update PanelLogin.js

--- src/PanelLogin.js
+++ src/PanelLogin.js
@@ -973,16 +973,7 @@ const handleAreaClick = (areaName) => {
   <ResponsiveContainer width="100%" height={200}>
     <LineChart 
       syncId="syncDias"
-      data={datosPromediosConTendencia.map(item => ({
-        ...item,
-        dia:
-          typeof item?.fecha === "string"
-            ? item.fecha.substring(0, 10)
-            : item?.fecha instanceof Date
-            ? item.fecha.toISOString().substring(0, 10)
-            : "",
-      }))}
-    >
+      data={datosPromediosConTendencia}>
       <CartesianGrid strokeDasharray="3 3" />
       <XAxis 
         dataKey="dia"
