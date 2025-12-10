00446e18758a11ce7de7eff30f71d88d9520e233
fernandoariel9-jpg <fernandoariel9@gmail.com>
2025-12-10 12:41:24 -0300

    Update TareasPersonal.js

--- src/TareasPersonal.js
+++ src/TareasPersonal.js
@@ -368,7 +368,32 @@ if (busqueda.trim()) {
     toast.success(`✅ Exportado en PDF (${nombreLista})`);
   };
 
-return (
+  return (
+   <>
+    {mostrarUsuarios && (
+      <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+        <div className="bg-white p-4 rounded-xl w-80 shadow-lg">
+          <h2 className="text-xl font-bold mb-3">Usuarios Registrados</h2>
+
+          <ul className="max-h-60 overflow-y-auto">
+            {usuarios.map((u) => (
+              <li key={u.id} className="border-b py-2">
+                <strong>{u.nombre}</strong>
+                <br />
+                <span className="text-gray-600 text-sm">{u.mail}</span>
+              </li>
+            ))}
+          </ul>
+
+          <button
+            onClick={() => setMostrarUsuarios(false)}
+            className="mt-3 bg-red-500 text-white px-3 py-1 rounded-xl w-full"
+          >
+            Cerrar
+          </button>
+        </div>
+      </div>
+    )}
     <div className="p-4 max-w-2xl mx-auto">
       <p
         className={`text-center mb-4 font-semibold ${
@@ -442,6 +467,8 @@ return (
     </div>
   </>
 );
+}
+
 {/* 🔍 Cuadro de búsqueda global */}
 <div className="relative my-3">
   <FaSearch className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
@@ -677,19 +704,3 @@ return (
     </div>
   );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
