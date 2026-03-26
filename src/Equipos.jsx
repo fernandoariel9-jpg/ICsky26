654c56e01343bc2072eb52037ceffc8f82057189
fernandoariel9-jpg <fernandoariel9@gmail.com>
2026-03-26 11:56:06 -0300

    Update Equipos.jsx

--- src/Equipos.jsx
+++ src/Equipos.jsx
@@ -50,8 +50,9 @@ export default function Equipos({ setVista, personal }) {
         subservicio: equipo.sub_servicio,
         asignado: personal.nombre,
         solicitado_por: personal.nombre,
-        solucion: observaciones,
-        origen: "interno"
+        origen: "interno",
+        tarea_id: tareaActiva?.id || null,
+        solucion: observaciones
       })
     });
 
