a15df23aa9956608673fe2d087e5ae865b70969c
fernandoariel9-jpg <fernandoariel9@gmail.com>
2026-04-16 09:06:37 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -273,6 +273,13 @@ function SupervisionWrapper({ switchToMenu, switchToRegistroUsuario, switchToReg
         >
           Registrar nuevo personal
         </button>
+
+        <button
+          onClick={() => window.location.href = "/estado-equipos"}
+          className="bg-purple-600 text-white px-4 py-2 rounded-xl w-full"
+        >
+          Ver estado de equipos
+        </button>
       </div>
     </div>
   ) : (
