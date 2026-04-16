f31085a6511a365bbdaca2a30416aabbc62d46f7
fernandoariel9-jpg <fernandoariel9@gmail.com>
2026-04-16 09:40:03 -0300

    Update PanelLogin.js

--- src/PanelLogin.js
+++ src/PanelLogin.js
@@ -554,7 +554,7 @@ const handleAreaClick = (areaName) => {
 
     <button
   onClick={() => window.open("https://icsky26.onrender.com/estado-equipos", "_blank")}
-  className="bg-purple-600 text-white px-4 py-2 rounded-xl w-full"
+  className="bg-purple-600 text-white px-4 py-2 rounded-xl rounded-x1 text_sm"
 >
   Ver estado de equipos
 </button>
