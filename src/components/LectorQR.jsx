221d49c00e4d3b941799a597bfb84235bbde909a
fernandoariel9-jpg <fernandoariel9@gmail.com>
2026-07-30 10:00:45 -0300

    Update LectorQR.jsx

--- src/components/LectorQR.jsx
+++ src/components/LectorQR.jsx
@@ -1,1 +1,20 @@
+        <QrReader
+          delay={300}
+          onError={handleError}
+          onScan={handleScan}
+          style={{ width: "100%" }}
+        />
 
+        <button
+          onClick={onCerrar}
+          className="mt-4 w-full bg-red-600 text-white rounded py-2"
+        >
+          Cancelar
+        </button>
+
+      </div>
+
+    </div>
+
+  );
+}
