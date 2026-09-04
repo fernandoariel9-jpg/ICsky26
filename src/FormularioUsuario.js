26b3522a2a18d9420f9208dd5a45e60a379ed25b
fernandoariel9-jpg <fernandoariel9@gmail.com>
2026-09-04 11:53:00 -0300

    Update FormularioUsuario.js

--- src/FormularioUsuario.js
+++ src/FormularioUsuario.js
@@ -286,7 +286,7 @@ export default function FormularioUsuario({ usuario, onLogout }) {
 
 return (
 <div className="p-4 max-w-2xl mx-auto">
-<img src="/public/logosmall_old.png" alt="Logo" className="mx-auto mb-4 w-12 h-auto" />
+<img src="/logosmall_old.png" alt="Logo" className="mx-auto mb-4 w-12 h-auto" />
 <h1 className="text-2xl font-bold mb-4 text-center">
 📌 Pedidos de tareas de{" "}
 <span className="text-blue-700">
