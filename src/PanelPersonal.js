757a9b50c2995575babb392a6eec98f77283b75f
fernandoariel9-jpg <fernandoariel9@gmail.com>
2026-09-10 13:22:42 -0300

    Integrar vista de prueba RIC25

--- src/PanelPersonal.js
+++ src/PanelPersonal.js
@@ -3,6 +3,7 @@ import TareasPersonal from "./TareasPersonal";
 import Equipos from "./Equipos";
 import SeleccionEquipo from "./SeleccionEquipo";
 import NuevoEquipo from "./NuevoEquipo";
+import RIC25 from "./RIC25";
 import RIC29 from "./RIC29";
 import RIC37 from "./RIC37";
 import RIC44 from "./RIC44";
@@ -36,6 +37,13 @@ export default function PanelPersonal({ personal, onLogout }) {
       {vista === "nuevoEquipo" && (
         <NuevoEquipo
           setVista={setVista}
+        />
+      )}
+
+      {vista === "ric25" && (
+        <RIC25
+          setVista={setVista}
+          personal={personal}
         />
       )}
 
