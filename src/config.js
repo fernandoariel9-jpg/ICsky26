ad49f22341498c7b52855a913e1c600b8c7cbf14
fernandoariel9-jpg <fernandoariel9@gmail.com>
2026-09-21 11:27:53 -0300

    Agregar endpoint RIC48

--- src/config.js
+++ src/config.js
@@ -23,6 +23,7 @@ export const API_URL = {
   Ric01: `${API_BASE_URL}/ric01`,
   Ric29: `${API_BASE_URL}/api/ric29`,
   Ric39: `${API_BASE_URL}/api/ric39`,
+  Ric48: `${API_BASE_URL}/api/ric39/ric48`,
   Ric44: `${API_BASE_URL}/api/ric44`,
   DiagnosticosRIC02: `${API_BASE_URL}/diagnosticos/ric02`,
   Guardias: `${API_BASE_URL}/api/guardias`,
